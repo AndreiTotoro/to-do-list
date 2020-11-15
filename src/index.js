70030fb4a0d12c0b9ece5d45e60146673885c44e
AndreiTotoro <andreitotoro@gmail.com>
2020-11-15 22:45:58 +0200

    Working on a button tester

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import {pageLoad} from './pageLoad'
 const defaultProject = projectFactory("Example Project", [toDoFactory("Example To Do1", "An example to do element", 12, "Low"), toDoFactory("Example To Do2", "An example to do element", 15, "Medium")])
 pageLoad()
 
+export {defaultProject}
+
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,19 +1,23 @@
 import {elementCreator} from "./elementCreator"
 import {taskMaker} from "./taskMaker"
+import {defaultProject} from "./index"
 import './cssReset.css'
 import './pageLoad.css'
 
+function buttonTest(){
+    taskMaker(defaultProject.name)
+}
 
 const domElements = {
     main: elementCreator().divMaker("main"),
     logo: elementCreator().elMaker("h1", "Andrew's To Do List", "logo"),
+    addTaskButton: elementCreator().elMaker("button", "Test Adding Thins", "testButton")
 }
 
 const pageLoad = () =>{
     elementCreator().bodyAppender(domElements.main)
     elementCreator().appender(domElements.main, domElements.logo)
-    taskMaker("test")
-    taskMaker("blahsdasda")
+    elementCreator().bodyAppender(domElements.addTaskButton)
 }
 
 export {pageLoad}
