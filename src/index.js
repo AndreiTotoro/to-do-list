b105d987521cf45ee3b24b7ec643d11241d7c08f
AndreiTotoro <andreitotoro@gmail.com>
2020-11-16 01:45:12 +0200

    Added default project mockup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import {projectFactory, toDoFactory} from "./factories"
 import {pageLoad} from './pageLoad'
 
-const defaultProject = projectFactory("Example Project", [toDoFactory("Example To Do1", "An example to do element", 12, "Low"), toDoFactory("Example To Do2", "An example to do element", 15, "Medium")])
+const defaultProject = projectFactory("Default Project", [])
 pageLoad()
+
 
 export {defaultProject}
 
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -9,21 +9,24 @@ import './pageLoad.css'
 const domElements = {
     main: elementCreator().divMaker("main"),
     logo: elementCreator().elMaker("h1", "Andrew's To Do List", "logo"),
-    
-}
-
-const newToDoButton = () =>{
-    const button = elementCreator().elMaker("button", "Add To Do", "newToDoButton")
-    button.addEventListener('click', () =>{
-        console.log("test")
+    newToDoButton: () =>{
+        const button = elementCreator().elMaker("button", "Add To Do", "newToDoButton")
+        button.addEventListener('click', () =>{
+        taskMaker(defaultProject.name)
     })
     return button
+    },
+    defaultProjectContainer: elementCreator().divMaker('defaultProject'),
+    defaultProjectTitle: elementCreator().elMaker("h1", "Default Project", "defaultProjectTitle")
 }
+
 
 const pageLoad = () =>{
     elementCreator().bodyAppender(domElements.main)
     elementCreator().appender(domElements.main, domElements.logo)
-    elementCreator().bodyAppender(newToDoButton())
+    elementCreator().bodyAppender(domElements.defaultProjectContainer)
+    elementCreator().appender(domElements.defaultProjectContainer, domElements.defaultProjectTitle)
+    elementCreator().appender(domElements.defaultProjectContainer, domElements.newToDoButton())
 }
 
 export {pageLoad}
