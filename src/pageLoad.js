a851691964ac538c02e02c657b0b3be647f77932
AndreiTotoro <andreitotoro@gmail.com>
2020-11-15 21:58:08 +0200

    Working on the button creator

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,13 +1,21 @@
 import {elementCreator} from "./elementCreator"
+import './cssReset.css'
+import './pageLoad.css'
 
 const domElements = {
     main: elementCreator().divMaker("main"),
-    logo: elementCreator().elMaker("h1", "Andrew's To Do List", "logo")
+    logo: elementCreator().elMaker("h1", "Andrew's To Do List", "logo"),
+    tasks: [{
+        taskCard: elementCreator().divMaker("taskCard"),
+        title: elementCreator().elMaker("h1", "Make everything automatic", "taskText"),
+    }]
 }
 
 const pageLoad = () =>{
     elementCreator().bodyAppender(domElements.main)
     elementCreator().appender(domElements.main, domElements.logo)
+    elementCreator().bodyAppender(domElements.tasks[0].taskCard)
+    elementCreator().appender(domElements.tasks[0].taskCard, domElements.tasks[0].title)
 }
 
 export {pageLoad}
